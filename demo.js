7f9009e5af8ab1c3802221ab88cdfc20b7e5ee4d
surajpandey05 <surajpandey9530937068@gmail.com>
2023-12-07 11:13:41 +0530

    commit 2

--- demo.js
+++ demo.js
@@ -1,7 +1,20 @@
 console.log("suraj")
 // adding two number
-let a=10;
-let b=25;
-let c=a+b;
-console.log(c);
-// 
+let a=10; // this is a ist variable
+let b=25;  // this is a 2nd variable
+let add=a+b;
+console.log(add);
+
+//subtraction two number
+let x=25;
+let y=15;
+let sub=x-y;
+console.log(sub);
+
+// division two number
+var first=55;
+var second=5;
+var quentant=first/second;
+console.log(quentant);
+
+
